4281fd9e5efa75972792677b9d4cf78af1e68718
ViktorWoll <viktorwoll@gmail.com>
2021-09-04 20:35:58 +0200

    created project with create react app and added files for homepage

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import HomePage from "./components/homepage.component";
+
+function App() {
+  return (
+    <div>
+
+    <HomePage/>
+
+
+    </div>
+
+  );
+}
+
+export default App;
